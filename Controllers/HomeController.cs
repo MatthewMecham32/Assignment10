b258f0f5205008c762f823452cc28d3c10bee8a4
Matthew Mecham <matthewmecham32@gmail.com>
2021-04-05 12:08:29 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Assignment10.Models;
+using Assignment10.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BowlingLeagueContext _context { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, BowlingLeagueContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public IActionResult Index(long? teamid,string teamname, int pageNum =0 )
+        {
+            int pageSize = 5;
+            return View(new IndexViewModel
+            {
+                Bowlers = (_context.Bowlers
+                .Where(b => b.TeamId == teamid || teamid == null)
+                .OrderBy(b => b.BowlerFirstName)
+                .Skip((pageNum - 1) * pageSize)
+                .Take(pageSize)
+                .ToList()
+                ),
+
+                PageNumberingInfo = new PageNumberingInfo
+                {
+                    NumItemsPerPage = pageSize,
+                    CurrentPage = pageNum,
+                    //If no team has been selected then get the full count. Else only get the count for the number of bowlers in a team
+                    TotalNumItems = (teamid == null ? _context.Bowlers.Count() :
+                    _context.Bowlers.Where(x => x.TeamId == teamid).Count())
+                },
+                TeamName = teamname
+            });
+
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Assignment10.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace Assignment10.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-info")]
+    public class PaginationTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlInfo;
+        public PaginationTagHelper (IUrlHelperFactory uhf)
+        {
+            urlInfo = uhf;
+        }
+
+        public PageNumberingInfo PageInfo { get; set; }
+        //public string TeamName { get; set; }
+
+        //Own dictionary key value pairs that we create
+        [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
+        public Dictionary<string, object> KeyValuePairs { get; set; } = new Dictionary<string, object>();
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagBuilder finishedTag = new TagBuilder("div");
+            IUrlHelper urlHelp = urlInfo.GetUrlHelper(ViewContext);
+
+            for(int i = 1; i <= PageInfo.NumPages; i++)
+            {
+                TagBuilder individualTag = new TagBuilder("a");
+
+                KeyValuePairs["pageNum"] = i;
+
+                individualTag.Attributes["href"] = urlHelp.Action("Index", KeyValuePairs) ;
+                individualTag.InnerHtml.AppendHtml(i.ToString());
+
+                finishedTag.InnerHtml.AppendHtml(individualTag); 
+            }
+
+            output.Content.AppendHtml(finishedTag.InnerHtml);
+        }
+    }
+}
